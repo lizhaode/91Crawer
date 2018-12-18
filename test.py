94f3598bf41a286eb482e146134db6fe7082431d
lizhaode <308697716@qq.com>
2018-12-18 16:32:58 +0800

    更改测试脚本

--- test.py
+++ test.py
@@ -1,20 +1,19 @@
 import os
 import unittest
 
-from main import Crawler91
+from CrawerImpl import Crawler91
 
 
 class TestCrawlerMethods(unittest.TestCase):
 
-    def test_normal_create_database(self):
-        if os.path.isfile('current91.db'):
-            os.remove('current91.db')
-        Crawler91().create_database()
-        assert os.path.isfile('current91.db'), 'create database failed'
+    def test_get_fake_headers(self):
+        headers = Crawler91().fake_headers()
+        assert headers, 'get fake headers failed'
 
-    def test_abnormal_create_database(self):
-        if not os.path.isfile('current91.db'):
-            with open('current91.db', 'w') as f:
-                f.write('test')
-        Crawler91().create_database()
-        assert os.path.isfile('current91.db'), 'create database failed'
+    def test_get_page_list(self):
+        page_num = Crawler91().get_page_list()
+        assert page_num 'get page number failed'
+
+    def test_download(self):
+        Crawler91().download('https://dldir1.qq.com/qqfile/qq/QQ9.0.8/24201/QQ9.0.8.24201.exe','qq.exe')
+        assert os.path.exists(qq.ext), 'download failed'
