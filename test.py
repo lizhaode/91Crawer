3195d5833367de8f6bf595020fb46285ba6898b0
lizhaode <308697716@qq.com>
2018-12-18 16:36:10 +0800

    fix test bug

--- test.py
+++ test.py
@@ -12,8 +12,8 @@ class TestCrawlerMethods(unittest.TestCase):
 
     def test_get_page_list(self):
         page_num = Crawler91().get_page_list()
-        assert page_num 'get page number failed'
+        assert page_num, 'get page number failed'
 
     def test_download(self):
-        Crawler91().download('https://dldir1.qq.com/qqfile/qq/QQ9.0.8/24201/QQ9.0.8.24201.exe','qq.exe')
-        assert os.path.exists(qq.ext), 'download failed'
+        Crawler91().download('https://dldir1.qq.com/qqfile/qq/QQ9.0.8/24201/QQ9.0.8.24201.exe', 'qq.exe')
+        assert os.path.exists('qq.exe'), 'download failed'
